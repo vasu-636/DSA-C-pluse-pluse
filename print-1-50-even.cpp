80ccaa7ab2e93c42ca03843c88c418d45b9ef4eb
vasu-636 <vasupatel632006@gmail.com>
2025-02-21 20:39:10 +0530

    4 added

---
+++ print-1-50-even.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+   int i = 1;
+   while (i <= 50)
+   {
+        if(i % 2 == 0){
+            cout << i << endl;
+            i++;     }
+   }
+   
+    return 0;
+}
