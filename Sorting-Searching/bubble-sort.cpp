064542857aa2106f3ab5c8b8803480322e120d73
vasu-636 <vasupatel632006@gmail.com>
2025-05-12 10:33:07 +0530

    sorting algorithm started

---
+++ Sorting-Searching/bubble-sort.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter the number of elements you want to enter in array : ";
+    cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n ; i++){
+        cout << "Enter the element  : ";
+        cin >> arr[i];
+    }
+
+    cout << "\n--------- Before Sorting ---------\n" ;
+
+    for(int value : arr){
+        cout << value << " ";
+    }
+    
+    for(int i = 0 ; i < n-1 ; i++){
+        for(int j = 0 ; j < n-i-1 ; j++){
+            if(arr[j] > arr[j+1]){
+                swap(arr[j], arr[j+1]);
+            }
+        }   
+    }
+
+    cout << "\n--------- After Sorting ---------\n" ;
+    for(int value : arr){
+        cout << value << " ";
+    }
+
+    return 0;
+}
---
+++ Sorting-Searching/insertion-sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+int main(){
+    int n;
+    cout << "Enter the number of elements you want to enter in array : ";
+    cin >> n;
+    vector<int> arr(n);
+
+    for(int i = 0; i < n ; i++){
+        cout << "Enter the element  : ";
+        cin >> arr[i];
+    }
+
+    cout << "\n--------- Before Sorting ---------\n" ;
+
+    for(int value : arr){
+        cout << value << " ";
+    }
+    
+    insertionSort(arr);
+
+    cout << "\n--------- After Sorting ---------\n" ;
+    for(int value : arr){
+        cout << value << " ";
+    }
+
+    return 0;
+}
