753a23700623c911b25d71a5b85065b1d27e98a4
vasu-636 <vasupatel632006@gmail.com>
2025-05-27 09:14:19 +0530

    code optimized

--- Linked-List-exam/q2.cpp
+++ Linked-List-exam/q2.cpp
@@ -1,131 +1,148 @@
 #include <iostream>
 using namespace std;
 
-class Node{
-    public:
-        int data;
-        Node* next;
-        Node(int data){
-            this->data = data;
-            this->next = nullptr;
-        }
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int data) {
+        this->data = data;
+        this->next = nullptr;
+    }
 };
-class LinkedList{
-    public : 
-        Node* head = nullptr;
-        int count = 0;
 
-        void addNode(int data){
-            Node* newNode = new Node(data);
-            newNode->next = head;
-            head = newNode;
-            count++;
+class LinkedList {
+public:
+    Node* head = nullptr;
+    int count = 0;
+
+    void addNode(int data) {
+        Node* newNode = new Node(data);
+        newNode->next = head;
+        head = newNode;
+        count++;
+    }
+
+    void viewAll() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    int getValueAtPosition(int pos) {
+        if (pos < 0 || pos >= count) {
+            cout << "Invalid position. Please enter between 0 and " << count - 1 << "." << endl;
+            return -1; // Return -1 as a sentinel for invalid
         }
 
-        void viewAll(){
+        Node* current = head;
+        int idx = 0;
+
+        while (current != nullptr) {
+            if (idx == pos) {
+                return current->data;
+            }
+            idx++;
+            current = current->next;
+        }
+
+        return -1; // This will not be reached if position is valid
+    }
+
+    void deleteAtPos(int pos) {
+        if (count == 0) {
+            cout << "Linked List is empty." << endl;
+            return;
+        }
+
+        if (pos < 0 || pos >= count) {
+            cout << "Invalid position." << endl;
+            return;
+        }
+
+        if (pos == 0) {
+            Node* toDelete = head;
+            head = head->next;
+            delete toDelete;
+        } else {
+            Node* prev = head;
+            for (int i = 0; i < pos - 1; i++) {
+                prev = prev->next;
+            }
+
+            Node* toDelete = prev->next;
+            prev->next = toDelete->next;
+            delete toDelete;
+        }
+
+        count--;
+        cout << "Node at position " << pos << " deleted." << endl;
+    }
+
+    ~LinkedList() {
+        while (head != nullptr) {
             Node* temp = head;
-            while(temp != nullptr){
-                cout << temp->data << " -> " ; 
-                temp = temp->next ;
-            }
-            cout << "NULL" << endl;
+            head = head->next;
+            delete temp;
         }
-
-        int getValueAtPosition(int pos){
-            if(pos < 0 || pos >= count){
-                cout << "Enter Valid Position..." << endl;
-                return 2;
-            }
-
-            int idx = 0;
-            Node* current = head;
-
-            while(current != nullptr){
-                if(idx == pos){
-                    return current->data;
-                }
-                idx ++;
-                current = current->next;
-            }
-            cout << "Value at " << pos << "index is " << current->data  << endl;
-        }
-
-        void deleteAtPos(int pos){
-            if(count == 0){
-                cout << "Linked List is empty .." << endl;
-                return ;
-            }
-
-            if(pos < 0 || pos >= count){
-                cout << "Invalid Position..." << endl;
-            }
-
-            if(pos == 0){
-                Node* toDelete = head;
-                head = head->next ;
-                delete toDelete;
-            }
-            else{
-                Node* prev = head;
-                for(int i = 0 ; i < (pos - 1) ; i++ ){
-                    prev = prev->next;
-                }
-
-                Node* toDelete = prev->next;
-                prev->next = toDelete->next;
-                delete toDelete;
-            }
-            count-- ; 
-            cout << "Node at Position " << pos << "deleted." << endl;
-        }
+    }
 };
-int main(){
+
+int main() {
     LinkedList list;
     int choice;
 
-    do
-    {
-        cout << "1. Add Node At Begin." << endl;
-        cout << "2. View All Node." << endl;
-        cout << "3. Search Node by Position." << endl;
-        cout << "4. Delete Node At any Position." << endl;
-        cout << "5. Exit." << endl;
-        cout << "Enter your choice : ";
+    do {
+        cout << "\n=== MENU ===\n";
+        cout << "1. Add Node at Beginning\n";
+        cout << "2. View All Nodes\n";
+        cout << "3. Search Node by Position\n";
+        cout << "4. Delete Node at Position\n";
+        cout << "5. Exit\n";
+        cout << "Enter your choice: ";
         cin >> choice;
 
-        switch (choice)
-        {
-            case 1 : {
+        switch (choice) {
+            case 1: {
                 int data;
-                cout << "Enter value you want to enter : " ;
+                cout << "Enter value to insert: ";
                 cin >> data;
                 list.addNode(data);
-            
                 break;
             }
-            case 2 : {
+            case 2: {
                 list.viewAll();
                 break;
             }
-            case 3 :{
+            case 3: {
                 int pos;
-                cout << "Enter position you want to search : ";
+                cout << "Enter position to search: ";
+                cin >> pos;
+                int value = list.getValueAtPosition(pos);
+                if (value != -1) {
+                    cout << "Value at position " << pos << " is " << value << endl;
+                }
+                break;
+            }
+            case 4: {
+                int pos;
+                cout << "Enter position to delete: ";
                 cin >> pos;
                 list.deleteAtPos(pos);
                 break;
             }
-            case 4 :{
-                int pos;
-                cout << "Enter position of node you want to delete : ";
-                cin >> pos;
-                list.deleteAtPos(pos);
+            case 5: {
+                cout << "Exiting..." << endl;
                 break;
             }
-            case 5 :{
-                break;
-            }
+            default:
+                cout << "Invalid choice. Try again." << endl;
         }
     } while (choice != 5);
-    
+
     return 0;
 }
